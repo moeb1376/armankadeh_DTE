105c57b499c5cf884024f6db98aea676e612cfed
tmoj <tmoj.1428@gmail.com>
2018-08-26 16:12:21 +0430

    forgot email template and completed login register

--- Login_page/js/login vuejs.js
+++ Login_page/js/login vuejs.js
@@ -1,61 +1,59 @@
+var global_data = new Vue({
+    data: {
+        seen: true,
+        page_number: 1,
+        relative_font_size: document.body.clientWidth
+    }
+});
 
 var loginform = new Vue({
     el: "#Login",
     data: {
-        password : '',
-        passwordFieldType : 'password',
-        "seen": true
+        password: '',
+        passwordFieldType: 'password'
     },
+
     methods: {
         change_pages: function () {
-            loginform.seen = false;
-            signupform.show = true
-
+            global_data.seen = false;
+            // alert(document.body.clientWidth)
         },
         show_hide_pass: function () {
             this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password'
         },
-        hide_pass:function () {
-            this.passwordFieldType = 'password'
+        new_pass_email: function () {
+            loginform.display1 = true
+        },
+        return_global: function () {
+            return global_data.seen;
+        },
+        return_global_size: function () {
+            return global_data.relative_font_size
         }
-    }
 
-})
+    }
+});
 
 var signupform = new Vue({
     el: "#SignUp",
     data: {
-        password : '',
-        re_password:'',
-        passwordFieldType : 'password',
-        repasswordFieldType : 'password',
-        "show": false
+        password: '',
+        re_password: '',
+        passwordFieldType: 'password',
+        repasswordFieldType: 'password',
     },
     methods: {
         change_pages1: function () {
-            loginform.seen = true;
-            signupform.show = false
-
+            global_data.seen = true;
         },
         show_hide_pass: function () {
             this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password'
         },
-        hide_pass:function () {
-            this.passwordFieldType = 'password'
-        },
         show_hide_repass: function () {
             this.repasswordFieldType = this.repasswordFieldType === 'password' ? 'text' : 'password'
         },
-        hide_repass:function () {
-            this.repasswordFieldType = 'password'
+        return_global: function () {
+            return global_data.seen
         }
-
     }
-})
-// const passwordField = document.querySelector('#password')
-//
-// function switchVisibility() {
-//     if (passwordField.getAttribute('type') === 'password') passwordField.setAttribute('type','text')
-//     else passwordField.setAttribute('type', 'password')
-//
-// }
+});
